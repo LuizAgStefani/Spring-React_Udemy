2b68fcdadac1c56814a7794cf0c7d66ac14adcf6
Luiz <lastefani@cliptech.com.br>
2021-08-27 12:34:03 -0300

    Arrumado problema de não buildar o app devido a main class

--- src/main/java/com/luiz/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/luiz/minhasfinancas/MinhasfinancasApplication.java
@@ -4,24 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
-public class MinhasfinancasApplication extends SpringBootServletInitializer implements WebMvcConfigurer{
+public class MinhasfinancasApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MinhasfinancasApplication.class, args);
     }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT","DELETE","OPTIONS");
-    }
-    
-    
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
